8ab72c286bf26f1a94edefb86c2a734d15e71917
kklee0928 <kevinklee86@gmail.com>
2017-09-07 23:48:44 -0700

    cleaned up unnecessary lines and added comments to explain the reason to setting the interval @ 1 millisecond.

--- clock.js
+++ clock.js
@@ -1,5 +1,4 @@
 var second = document.getElementById("second");
-	console.log(document.getElementById('second'));
 var minute = document.getElementById("minute");
 var hour = document.getElementById("hour");
 
@@ -16,7 +15,6 @@ var tickHour = function () {
 
 	hour.style.transform = "rotate(" + degreesHou + "deg)";
 }
-//need logic to move the second hand using the setInterval calling the tickSecond function
 var tickMinute = function () {
 	if (degreesMin < 360) {
 		degreesMin += 6;
@@ -26,9 +24,9 @@ var tickMinute = function () {
 
 	tickHour();
 	minute.style.transform = "rotate(" + degreesMin + "deg)";
-	//tickMinute === setInterval(tickSecond, 2000)
+	
 }
-
+//need logic to move the second hand using the setInterval calling the tickSecond function
 var tickSecond = function() {
 	if (degreesSec < 360) {
 		degreesSec += 6;
@@ -38,17 +36,19 @@ var tickSecond = function() {
 	}
 	
 	second.style.transform = "rotate(" + degreesSec + "deg)";
-		
 }
-//this will call tickSeconds to move every second
+//this will call tickSeconds to move every second when the browser reads the end of the page, starting the clock tick.
 
 setInterval(tickSecond, 1);
-//now set the minute hand to do that same. 
-//something to think about, will the minute hand have the same
+//the interval was set to 1 to check if the hour hand would tick in correct increments. Set to 1000 in order to go at the correct sec-min-hour interval.
+
+//now set the minute hand to do the same. 
+//something to think about; will the minute hand have the same
 //logic or can I use the tickSecond function to initiate the minute 
 //logic?
 
 //	1 minute === 60 seconds. when 60 seconds makes 360 deg minute makes a 6 degree rotation.
 
+//questions to ask myself: 
 
 
